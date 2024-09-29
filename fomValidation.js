5437b0bf32493dcf7b1a521de52f287a8eb8bb4e
sofia <sofia.loria9@gmail.com>
2024-09-28 21:35:01 -0300

    form validation changes

--- fomValidation.js
+++ fomValidation.js
@@ -3,18 +3,17 @@ document.getElementById('registroform').addEventListener('submit', function (eve
     let experiencia = document.querySelector('input[name="experiencia"]:checked');
     let comida = document.querySelector('input[name="comida"]:checked');
 
+    // Validación
     if (!nombre) {
         document.getElementById('nombreerror').textContent = 'El nombre es obligatorio';
         event.preventDefault(); // Detener el envío
-    }
-
-    if (!experiencia) {
+    } else if (!experiencia) {
         alert('Debes puntuar la experiencia.');
         event.preventDefault(); // Detener el envío
-    }
-
-    if (!comida) {
+    } else if (!comida) {
         alert('Debes puntuar la comida.');
         event.preventDefault(); // Detener el envío
+    } else {
+        alert('¡Gracias por responder! Te esperamos pronto!');
     }
 });
